3c19f566fb51e12d34df8fad1c21b2c71410300a
deanpreese <deanpreese66@gmail.com>
2024-06-03 09:24:17 -0700

    Models

--- Strategy.EXP/c_gan_lstm.py
+++ Strategy.EXP/c_gan_lstm.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import random
-from tensorflow.keras.layers import Input, LSTM, Concatenate, Reshape, Flatten, Dense, LeakyReLU, Dropout, BatchNormalization, Layer,  Attention, Bidirectional
+from tensorflow.keras.layers import Input, LSTM, Concatenate, Reshape, Flatten, Dense, LeakyReLU, Dropout, BatchNormalization, Layer,  Attention, Bidirectional, TimeDistributed, Conv1D
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.initializers import RandomNormal
@@ -32,8 +32,13 @@ def create_sequences(data, sequence_length):
 # Function to create the generator model with LSTM
 def create_generator(input_dim, conditioning_dim, lay1, lay2, lay3, sequence_length):
     init = RandomNormal(stddev=0.02)
-    input_layer = Input(shape=(sequence_length, input_dim + conditioning_dim))
     
+    input_layer = Input(shape=(sequence_length, input_dim + conditioning_dim))
+    print("Input Shape:", input_layer.shape)
+
+    x = TimeDistributed(Conv1D(filters=32, kernel_size=3, activation='relu', padding='same'))(input_layer)
+    print("After Conv1D Shape:", x.shape)
+
     x = LSTM(lay1, return_sequences=True, kernel_initializer=init)(input_layer)
     #x = LeakyReLU(negative_slope=0.2)(x)
     #x = BatchNormalization()(x)
@@ -59,6 +64,9 @@ def create_generator(input_dim, conditioning_dim, lay1, lay2, lay3, sequence_len
 def create_discriminator(input_dim, conditioning_dim, lay1, lay2, lay3, sequence_length):
     init = RandomNormal(stddev=0.02)
     input_layer = Input(shape=(sequence_length, input_dim + conditioning_dim))
+    
+    x = TimeDistributed(Conv1D(filters=32, kernel_size=3, activation='relu'))(input_layer)
+
     
     x = LSTM(lay1, return_sequences=True, kernel_initializer=init)(input_layer)
     #x = LeakyReLU(negative_slope=0.2)(x)
