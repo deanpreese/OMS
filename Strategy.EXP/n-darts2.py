2f1e205a98208dafcd55e6dd227e87506354393a
deanpreese <deanpreese66@gmail.com>
2024-06-14 15:58:51 -0700

    darts

--- Strategy.EXP/n-darts2.py
+++ Strategy.EXP/n-darts2.py
@@ -30,7 +30,11 @@ def process_data(data, feature_columns, target_column, split):
     return X_train, X_test, y_train, y_test, scaler
 
 # Plot results
-def plot_results(actuals, predictions):
+def plot_results(actuals, predictions, last_x_rows=100):
+    
+    model_predictions = model_predictions[-last_x_rows:]
+    y_test = y_test[-last_x_rows:]   
+    
     plt.figure(figsize=(12, 6))
     plt.plot(actuals, label='Actual')
     plt.plot(predictions, label='Predicted')
@@ -126,15 +130,15 @@ def main():
         #'RSI',
         #'ATR2',
         'ATR21',
-        #'ATR3',
+        'ATR3',
         'ATR31', 
         'ATR32',
         'ATR34',   
-        #'ROC',     
-        #'SDKC9',   
+        'ROC',     
+        'SDKC9',   
         'SDKC91',  
         'SDBB91',  
-        #'SDLR310'
+        'SDLR310'
     ]
 
 
@@ -143,8 +147,8 @@ def main():
     feature_columns = list(data.columns[:-1])
     
     target_column = 'output'  # Replace with your actual target column name
-    input_chunk_length = 60
-    output_chunk_length = 10
+    input_chunk_length = 15
+    output_chunk_length = 3
     n_epochs = 1000
     num_stacks = 3
     num_blocks = 2
@@ -166,13 +170,15 @@ def main():
     # Make predictions
     print("Making predictions...")
     predictions = model.predict(1, series=y_test, past_covariates=X_test)
-    #print("Generating statistics...")
-    #generate_statistics(test_series, predictions)
+    print("Generating statistics...")
+    generate_statistics(y_test, predictions)
     
     # Inverse transform the predictions and actual values
     #actual_values = scaler.inverse_transform(y_test).values()
     #predicted_values = scaler.inverse_transform(predictions).values()
-    plot_results(y_test, predictions)
+    
+    print("Plotting results...")
+    plot_results(y_test, predictions, 250)
 
 if __name__ == "__main__":
     main()
